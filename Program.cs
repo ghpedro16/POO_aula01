f808d63fac29418e3e7e6c828f39b7b2145d74d5
Pedro Henrique <pedrohenrique.phas16@gmail.com>
2025-09-09 00:38:25 +0000

    atividade televisao finalizada

--- Program.cs
+++ Program.cs
@@ -1,8 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 
 Televisao tv = new Televisao(55f);
+
+tv.LigarTv(false);
 Console.WriteLine($"A TV tem o tamanho {tv.Tamanho}");
+
+tv.GerenciarCanais(30);
+Console.WriteLine($"Canal {tv.Canal}");
+
+tv.GerenciarVolume(50);
+Console.WriteLine($"Volume {tv.Volume}");
 
 tv.AumentarVolume();
 Console.WriteLine($"Volume {tv.Volume}");
+
+tv.AumentarVolume();
+Console.WriteLine($"Volume {tv.Volume}");
+
+tv.DiminuirVolume();
+Console.WriteLine($"Volume {tv.Volume}");
+
+tv.ProximoCanal();
+Console.WriteLine($"Canal {tv.Canal}");
+
+tv.AtivarMudo(true);
+
+
+
+
--- Televisao.cs
+++ Televisao.cs
@@ -1,6 +1,9 @@
 public class Televisao{
 
     private const int VOL_MAX = 100;
+    private const int VOL_MIN = 0;
+    private const int CANAL_MAX = 520;
+    private const int CANAL_MIN = 1;
     public Televisao(float tamanho){
         Tamanho = tamanho;
     }
@@ -10,6 +13,16 @@ public class Televisao{
     public int Volume {get; private set;}
     public int Canal {get; set;}
     public bool Estado {get; set;}
+    public bool Mudo {get; set;}
+
+    public int GerenciarVolume(int volume){
+        Volume = volume;
+        if(volume > VOL_MAX || volume < VOL_MIN){
+            Console.WriteLine("Volume máximo atingido!");
+        }
+
+        return Volume;
+    }
 
     public void AumentarVolume(){
         if(Volume < VOL_MAX){
@@ -17,7 +30,57 @@ public class Televisao{
         } else {
             Console.WriteLine("TV já está no volume máx.");
         }
-        
+    }
+
+    public void DiminuirVolume(){
+        if(Volume > VOL_MIN){
+            Volume--;
+        } else{
+            Console.WriteLine("TV está no mudo!");
+        }
+    }
+
+    public int GerenciarCanais(int canal){
+        Canal = canal;
+        if(Canal < 0 || Canal > 520){
+            Console.WriteLine("Este canal não existe!");
+        }
+
+        return Canal;
+    }
+
+    public void ProximoCanal(){
+        if(Canal < CANAL_MAX){
+            Canal++;
+        } else{
+            Console.WriteLine("Não existem mais canais!");
+        }
+    }
+
+    public void CanalAnterior(){
+        if(Canal > CANAL_MIN){
+            Canal--;
+        } else{
+            Console.WriteLine("Não existem mais canais!");
+        }
+    }
+
+    public void LigarTv(bool Estado){
+        if(Estado == true){
+            Console.WriteLine("A TV está ligada!");
+        } else{
+            Console.WriteLine("Ligue a televisão!");
+            Resolucao = 0;
+            Canal = 0;
+            Volume = 0;
+        }
+    }
+
+    public void AtivarMudo(bool Mudo){
+        if(Mudo == true){
+            Volume = VOL_MIN;
+            Console.WriteLine("TV está no mudo!");
+        }
     }
 
 
