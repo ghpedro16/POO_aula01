f00b199cd8bcad60600f129ce265e6fc57252127
Pedro Henrique <pedrohenrique.phas16@gmail.com>
2025-09-16 00:32:53 +0000

    classe cartao debito alterada

--- heranca/CartaoDebito.cs
+++ heranca/CartaoDebito.cs
@@ -1,19 +1,40 @@
-public class CartaoDebito{
+public class CartaoDebito
+{
 
-    public CartaoDebito(){
+    public CartaoDebito()
+    {
         Bandeira = BandeiraCartao.Visa;
     }
 
-    public string Numero {get; set;}
-    public BandeiraCartao Bandeira {get; set;}
-    public DateTime Vencimento {get; set;}
-    public string Cvv {get; set;}
+    public string Numero { get; set; }
+    public BandeiraCartao Bandeira { get; set; }
+    public DateTime Vencimento { get; set; }
+    public string Cvv { get; set; }
     public string Portador { get; set; }
+
+    public decimal Saldo { get; set; }
+    public decimal Limite { get; set; }
+
+    public bool Debitar(decimal valor)
+    {
+        if (valor > Saldo)
+            return false;
+
+        Saldo -= valor;
+        return true;
+    }
+
+    public bool Debitar(decimal valor, bool usarLimite)
+    {
+        
+    }
 }
 
-public enum BandeiraCartao{
+public enum BandeiraCartao
+{
     Visa,
     Mastercard,
     Amex,
     Elo
 }
+
